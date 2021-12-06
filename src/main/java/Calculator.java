d35c5561c49d9bf80521b2330a204a38b3add264
abzilla786 <abdullah.ayyaz66@gmail.com>
2021-12-06 13:59:33 +0000

    break code

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,7 +1,7 @@
 public class Calculator {
 
     public int addition(int num1, int num2){
-        return num1 + num2;
+        return num1 - num2;
     }
 
     public int subtraction(int num1, int num2){
